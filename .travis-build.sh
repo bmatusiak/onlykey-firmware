758cb0b9e72be059c5dc6f7935b8431b45dbaf23
bmatusiak <bmatusiak@gmail.com>
2020-07-04 21:38:43 -0400

    update

--- .travis-build.sh
+++ .travis-build.sh
@@ -7,8 +7,11 @@
 
 if [ "${1}" == "1" ] ; then
     echo "BUILD TOOLS"
-    
-    
+    git clone https://github.com/bmatusiak/arduino-1.6.5-r5-teensy_127 ./builder
+    cd ./builder
+    make docker-build-toolchain
 else
     echo "BUILD"
+    cd ./builder
+    make docker-build
 fi
